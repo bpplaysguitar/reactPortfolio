aa234eddf37d325a6885850f4d5cf3977aade8b9
Brian Palay <bpplaysguitar@hotmail.com>
2021-09-05 21:44:50 -0500

    footer icons

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -2,26 +2,30 @@ import React from 'react';
 
 function Footer() {
   return (
-    <div className="mt-5 py-4 footer">
-      <div className="d-flex flex-column text-center container">
+    <div className="mt-5 footer">
+      <div className="container">
       <div className="col-12">
-        <ul className="nav row text-center d-flex">
-            <li className="nav-item col-4 me-3"> <i className="me-1 far fa-copyright"></i>2021 Brian Palay</li>
-          <li className="nav-item mb-4 col-1 me-2">
+        <ul className="nav py-4 row d-flex align-items-center text-center">
+            <li className="me-5 nav-item col-4 d-flex justify-content-center align-items-center"> <i className="me-1 far fa-copyright"></i>2021 Brian Palay</li>
+          <li className="nav-item d-flex justify-content-center align-items-center col-1 ms-3 ">
             <a className="d-flex flex-column connect-icon text-decoration-none nowrap" href="https://www.linkedin.com/in/brianpalay"
-              target="_blank" rel="noreferrer"><i className="mb-2 fab fa-2x fa-linkedin"></i></a>
+              target="_blank" rel="noreferrer"><i className="fab fa-2x fa-linkedin"></i></a>
           </li>
-          <li className="nav-item mb-4 col-1 me-2">
+          <li className="nav-item d-flex justify-content-center align-items-center col-1 ms-2 ">
             <a className="d-flex flex-column connect-icon text-decoration-none nowrap" href="https://github.com/bpplaysguitar"
-              target="_blank" rel="noreferrer"><i className="mb-2 fab fa-2x fa-github-square"></i></a>
+              target="_blank" rel="noreferrer"><i className="fab fa-2x fa-github-square"></i></a>
           </li>
-          <li className="nav-item mb-4 col-1 me-2">
+          <li className="nav-item d-flex justify-content-center align-items-center col-1 ms-2 ">
+            <a className="d-flex flex-column connect-icon text-decoration-none nowrap" href="https://www.facebook.com/brianpalay"
+              target="_blank" rel="noreferrer"><i class="fa-2x fab fa-facebook-square"></i></a>
+          </li>
+          <li className="nav-item d-flex justify-content-center align-items-center col-1 ms-2 ">
             <a className="d-flex flex-column connect-icon text-decoration-none nowrap" href="mailto:brianpalay@gmail.com"><i
-              className="mb-2 fas fa-2x fa-envelope-square"></i></a>
+              className="fas fa-2x fa-envelope-square"></i></a>
             </li>
-            <li className="nav-item mb-4 col-1 me-2">
+            <li className="nav-item d-flex justify-content-center align-items-center col-1 ms-2 ">
               <a className="d-flex flex-column connect-icon text-decoration-none nowrap" href="tel:612-202-5710">
-                <i className="mb-2 fas fa-2x fa-phone-square"></i></a>
+                <i className="fas fa-2x fa-phone-square"></i></a>
             </li>
           </ul>
       </div>
