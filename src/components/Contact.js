490549c7662f653bee3cb4d0bcb832ed452e7ea6
Brian Palay <bpplaysguitar@hotmail.com>
2021-09-05 20:25:29 -0500

    footer with links

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -13,29 +13,8 @@ function Contact() {
     ">
     <div className="row container">
       <Form />
-      <div className="col-12">
-        <ul className="nav row text-center d-flex">
-          <li className="nav-item mb-4 col-lg-3 col-md-6 col-sm-6 col-12">
-            <a className="d-flex flex-column connect-icon text-decoration-none nowrap" href="https://www.linkedin.com/in/brianpalay"
-              target="_blank" rel="noreferrer"><i className="mb-2 fab fa-3x fa-linkedin"></i>linkedin.com/in/brianpalay</a>
-          </li>
-          <li className="nav-item mb-4 col-lg-3 col-md-6 col-sm-6 col-12">
-            <a className="d-flex flex-column connect-icon text-decoration-none nowrap" href="https://github.com/bpplaysguitar"
-              target="_blank" rel="noreferrer"><i className="mb-2 fab fa-3x fa-github-square"></i>github.com/bpplaysguitar</a>
-          </li>
-          <li className="nav-item mb-4 col-lg-3 col-md-6 col-sm-6 col-12">
-            <a className="d-flex flex-column connect-icon text-decoration-none nowrap" href="mailto:brianpalay@gmail.com"><i
-              className="mb-2 fas fa-3x fa-envelope-square"></i>brianpalay@gmail.com</a>
-            </li>
-            <li className="nav-item mb-4 col-lg-3 col-md-6 col-sm-6 col-12">
-              <a className="d-flex flex-column connect-icon text-decoration-none nowrap" href="tel:612-202-5710">
-                <i className="mb-2 fas fa-3x fa-phone-square"></i>612-202-5710</a>
-            </li>
-          </ul>
-      </div>
     </div>
   </section>
-      
     </section>
     </div>
   );
--- src/components/Footer.js
+++ src/components/Footer.js
@@ -4,7 +4,27 @@ function Footer() {
   return (
     <div className="mt-5 py-4 footer">
       <div className="d-flex flex-column text-center container">
-        <h6><i className="me-1 far fa-copyright"></i>2021 Brian Palay</h6>
+      <div className="col-12">
+        <ul className="nav row text-center d-flex">
+            <li className="nav-item col-4 me-3"> <i className="me-1 far fa-copyright"></i>2021 Brian Palay</li>
+          <li className="nav-item mb-4 col-1 me-2">
+            <a className="d-flex flex-column connect-icon text-decoration-none nowrap" href="https://www.linkedin.com/in/brianpalay"
+              target="_blank" rel="noreferrer"><i className="mb-2 fab fa-2x fa-linkedin"></i></a>
+          </li>
+          <li className="nav-item mb-4 col-1 me-2">
+            <a className="d-flex flex-column connect-icon text-decoration-none nowrap" href="https://github.com/bpplaysguitar"
+              target="_blank" rel="noreferrer"><i className="mb-2 fab fa-2x fa-github-square"></i></a>
+          </li>
+          <li className="nav-item mb-4 col-1 me-2">
+            <a className="d-flex flex-column connect-icon text-decoration-none nowrap" href="mailto:brianpalay@gmail.com"><i
+              className="mb-2 fas fa-2x fa-envelope-square"></i></a>
+            </li>
+            <li className="nav-item mb-4 col-1 me-2">
+              <a className="d-flex flex-column connect-icon text-decoration-none nowrap" href="tel:612-202-5710">
+                <i className="mb-2 fas fa-2x fa-phone-square"></i></a>
+            </li>
+          </ul>
+      </div>
       </div>
   </div> 
   );
