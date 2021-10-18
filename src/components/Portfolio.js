8602751e3fc5810a05d7e275a42eeda74393a740
Brian Palay <bpplaysguitar@hotmail.com>
2021-10-18 16:59:09 -0500

    added EnviroPact

--- src/components/Portfolio.js
+++ src/components/Portfolio.js
@@ -13,48 +13,57 @@ const projectItems = [
   },
   {
     id: 2,
+    name: "EnviroPact",
+    languages: "JavaScript, React, Apollo, Material UI, GraphQL, Express, Mongoose",
+    deployedDemoBtnText: "Deployed",
+    deployedDemoLink: "https://enviropact.herokuapp.com/",
+    repository: "https://github.com/Justinean/EnviroPact",
+    backgroundImg:"project-image2"
+  },
+  {
+    id: 3,
     name: "Bike2Work",
     languages: "JavaScript, API, HTML, Bulma, CSS",
     deployedDemoBtnText: "Deployed",
     deployedDemoLink: "https://bpplaysguitar.github.io/Bike2Work/",
     repository: "https://github.com/bpplaysguitar/Bike2Work",
-    backgroundImg:"project-image2"
+    backgroundImg:"project-image3"
   },
   {
-    id: 3,
+    id: 4,
     name: "README Generator",
     languages: "JavaScript, Node.js, Inquirer.js",
     deployedDemoBtnText: "Demo",
     deployedDemoLink: "https://youtu.be/S1CyQbMa4A4",
     repository: "https://github.com/bpplaysguitar/readme-generator",
-    backgroundImg:"project-image3"
+    backgroundImg:"project-image4"
   },
   {
-    id: 4,
+    id: 5,
     name: "Employee Tracker",
     languages: "JavaScript, Node.js, Inquirer.js, MySQL, Sequelize",
     deployedDemoBtnText: "Demo",
     deployedDemoLink: "https://youtu.be/vFqxlpn9C-U",
     repository: "https://github.com/bpplaysguitar/employeeTracker",
-    backgroundImg:"project-image4"
+    backgroundImg:"project-image5"
   },
   {
-    id: 5,
+    id: 6,
     name: "Team Profile Generator",
     languages: "JavaScript, Node.js, Inquirer.js, HTML, CSS",
     deployedDemoBtnText: "Demo",
     deployedDemoLink: "https://youtu.be/eCcu1BVvk48",
     repository: "https://github.com/bpplaysguitar/teamProfileGenerator",
-    backgroundImg:"project-image5"
+    backgroundImg:"project-image6"
   },
   {
-    id: 6,
+    id: 7,
     name: "JavaScript Fundamentals Quiz",
     languages: "HTML, CSS, JavaScript",
     deployedDemoBtnText: "Deployed",
     deployedDemoLink: "https://bpplaysguitar.github.io/javascript-fundamentals-quiz/",
     repository: "https://github.com/bpplaysguitar/javascript-fundamentals-quiz",
-    backgroundImg:"project-image6"
+    backgroundImg:"project-image7"
   }
 ];
 
