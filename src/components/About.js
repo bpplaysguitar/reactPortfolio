aabce35e5089581deef2ac109d306839e12d128f
Brian Palay <bpplaysguitar@hotmail.com>
2023-03-12 18:05:52 -0500

    update about and footer

--- src/components/About.js
+++ src/components/About.js
@@ -8,7 +8,22 @@ function About() {
    <section className="container my-5">
       <h2 className="container pb-3 border-bottom" id="aboutme">About Me</h2>
       <p className="pt-2">
-        I’m Brian, and I’m a web developer and UX designer. I strive to provide users with easy and enjoyable experiences through clean, uncluttered design that must first be intuitive before it can be beautiful. I started my design journey with a degree in architecture, then later translated my passion from physical spaces to web applications by earning certificates in UX design from Google, and full-stack web development from the University of Minnesota.
+        Hi, my name is Brian and I'm a UX designer and web developer (specializing in font-end development). I'm currently working for 3M in their health care business group, where I support marketing campaigns and patient resources related to wound care and surgical incision management products.
+      </p>
+      <p className="pt-2">
+        But let's back up a little.
+      </p>
+      <p className="pt-2">
+        After graduating from the University of Minnesota with a degree in architecture, I spent over a decade as primarily a professional musician, playing guitar, singing, and writing songs, working with countless artists on stages and in studios, in Minneapolis and Nashville. During this time, I also worked in various design-based capacities, from graphic design to 3D modeling. Yes, a musician who also values stability, which is probably why you're reading this.
+      </p>
+      <p className="pt-2">
+        In 2020, when the world, especially the entertainment industry, became anything but stable, I took a look around and realized it wasn't what I wanted anymore, and I was inspired by my friends in the tech industry. I had a new dream. So not really knowing how a webpage becomes a webpage, I explored YouTube for a few months, then enrolled in the University of Minnesota full-stack coding boot camp, a pretty insanely intense six-month takeover of my entire life, and the lives of all my classmates.
+      </p>
+      <p className="pt-2">
+        It was in that boot camp that I realized how fun front-end development was, and how much it tied into my undergraduate studies in architecture as well as all the years I had be thinking with a design-centered mind since then. I doubled down on my interest in the front-end experience by enrolling in Google's UX design certification course as soon as I completed the web development boot camp. Now I'm able to enjoy both the design and development stages of the experience.
+      </p>
+      <p className="pt-2">
+        When I'm not working, I do still like to get out and play some gigs, record music parts at home to send to other musicians all over the world pretty regularly, work out, find the best dark chocolate bars, and renovate my unique and modern 1990s house, a process which has caused me to learn so many skills I never intended to know.
       </p>
     </section>
     </div>
--- src/components/Footer.js
+++ src/components/Footer.js
@@ -6,7 +6,7 @@ function Footer() {
       <div className="container">
       <div className="col-12">
         <ul className="d-inline nav py-4 row d-flex align-items-center text-center">
-            <li className="d-inline me-2 col-5 d-flex justify-content-center align-items-center"> <span className="nowrap"><i className="me-1 far fa-copyright"></i>2021 Brian Palay</span></li>
+            <li className="d-inline me-2 col-5 d-flex justify-content-center align-items-center"> <span className="nowrap"><i className="me-1 far fa-copyright"></i>2023 Brian Palay</span></li>
           <li className="nav-item d-flex justify-content-center align-items-center ms-2 col-1">
             <a className="d-flex flex-column connect-icon text-decoration-none nowrap" href="https://www.linkedin.com/in/brianpalay"
               target="_blank" rel="noreferrer"><i className="fab fa-2x fa-linkedin"></i></a>
