ed33794fea19e019824a846749e8cec2bc137f6b
Brian Palay <bpplaysguitar@hotmail.com>
2023-03-12 20:18:53 -0500

    added portfolio

--- src/components/Portfolio.js
+++ src/components/Portfolio.js
@@ -1,27 +1,12 @@
 import React from 'react';
 import Project from './Project';
 
+
+
+
 const projectItems = [
   {
     id: 1,
-    name: "Manage Me Now",
-    languages: "JavaScript, Node.js, MySQL, Sequelize, Handlebars.js, Bootstrap",
-    deployedDemoBtnText: "Deployed",
-    deployedDemoLink: "https://manage-me-now.herokuapp.com/login",
-    repository: "https://github.com/bpplaysguitar/Manage-Me-Now",
-    backgroundImg:"project-image1"
-  },
-  {
-    id: 2,
-    name: "EnviroPact",
-    languages: "JavaScript, React, Apollo, Material UI, GraphQL, Express, Mongoose",
-    deployedDemoBtnText: "Deployed",
-    deployedDemoLink: "https://enviropact.herokuapp.com/",
-    repository: "https://github.com/bpplaysguitar/EnviroPact",
-    backgroundImg:"project-image2"
-  },
-  {
-    id: 3,
     name: "Bike2Work",
     languages: "JavaScript, API, HTML, Bulma, CSS",
     deployedDemoBtnText: "Deployed",
@@ -30,7 +15,7 @@ const projectItems = [
     backgroundImg:"project-image3"
   },
   {
-    id: 4,
+    id: 2,
     name: "README Generator",
     languages: "JavaScript, Node.js, Inquirer.js",
     deployedDemoBtnText: "Demo",
@@ -39,7 +24,16 @@ const projectItems = [
     backgroundImg:"project-image4"
   },
   {
-    id: 5,
+    id: 3,
+    name: "JavaScript Fundamentals Quiz",
+    languages: "HTML, CSS, JavaScript",
+    deployedDemoBtnText: "Deployed",
+    deployedDemoLink: "https://bpplaysguitar.github.io/javascript-fundamentals-quiz/",
+    repository: "https://github.com/bpplaysguitar/javascript-fundamentals-quiz",
+    backgroundImg:"project-image7"
+  },
+  {
+    id: 4,
     name: "Employee Tracker",
     languages: "JavaScript, Node.js, Inquirer.js, MySQL, Sequelize",
     deployedDemoBtnText: "Demo",
@@ -48,22 +42,13 @@ const projectItems = [
     backgroundImg:"project-image5"
   },
   {
-    id: 6,
+    id: 5,
     name: "Team Profile Generator",
     languages: "JavaScript, Node.js, Inquirer.js, HTML, CSS",
     deployedDemoBtnText: "Demo",
     deployedDemoLink: "https://youtu.be/eCcu1BVvk48",
     repository: "https://github.com/bpplaysguitar/teamProfileGenerator",
     backgroundImg:"project-image6"
-  },
-  {
-    id: 7,
-    name: "JavaScript Fundamentals Quiz",
-    languages: "HTML, CSS, JavaScript",
-    deployedDemoBtnText: "Deployed",
-    deployedDemoLink: "https://bpplaysguitar.github.io/javascript-fundamentals-quiz/",
-    repository: "https://github.com/bpplaysguitar/javascript-fundamentals-quiz",
-    backgroundImg:"project-image7"
   }
 ];
 
@@ -72,6 +57,7 @@ function Portfolio() {
     <div>
       <section className="container my-5">
       <h2 className="container pb-3 border-bottom" id="portfolio">Portfolio</h2>
+      <h5 className="container mt-3">Web Development</h5>
     <Project projectItems={projectItems} />
     </section>
     </div>
